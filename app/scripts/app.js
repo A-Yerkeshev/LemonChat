58ff369177f4d107ed7197198cebd500890f37aa
Arman Yerkesh <a.yerkesh@gmail.com>
2018-11-07 18:41:42 +0300

    Reinitialize all files

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name chatApp
+ * @description
+ * # chatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('chatApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
