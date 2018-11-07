3343c411de843f63e7cb3840c4f1d3e256b77e9f
Arman Yerkesh <a.yerkesh@gmail.com>
2018-11-07 14:05:41 +0300

    Change module invocation in beforeEach in main.js

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,9 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('chatApp'));
+  beforeEach(function() {
+    module('chatApp');
+  });
 
   var MainCtrl,
     scope;
