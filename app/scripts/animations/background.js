6f02657c525ff7f58ff8da5558f335a3ee2a94dc
Arman Yerkesh <a.yerkesh@gmail.com>
2018-11-13 12:49:03 +0300

    Add background animation

--- app/scripts/animations/background.js
+++ app/scripts/animations/background.js
@@ -1,31 +1,59 @@
 (function animateBackground() {
   setInterval(function () {
     var circle = document.createElement('div');
+    var innerCircle = document.createElement('div');
 
     circle.classList.add('circle');
+    innerCircle.classList.add('inner-circle');
 
-    var size = (Math.random()*30+10).toFixed();
-    var posx = (Math.random() * window.innerWidth - size * 2).toFixed();
-    var posy = (Math.random() * window.innerHeight - size * 2).toFixed();
+    var size = Math.random()*30+20;
+    var posx = Math.random() * window.innerWidth;
+    var posy = Math.random() * window.innerHeight;
 
-    circle.style.width = size + '%';
-    circle.style.height = size + '%';
     circle.style.left = posx + 'px';
     circle.style.top = posy + 'px';
+    innerCircle.style.left = posx + 'px';
+    innerCircle.style.top = posy + 'px';
 
     document.body.appendChild(circle);
+    document.body.appendChild(innerCircle);
 
     anime({
       targets: circle,
-      width: size * 2 + '%',
-      height: size * 2 + '%',
-      duration: 10000,
- //     easing: 'easeOutBack'
+      width: size + '%',
+      height: size + '%',
+      left: (posx-window.innerWidth*size/200) + 'px',
+      top: (posy-window.innerHeight*size/200) + 'px',
+      duration: 20000
+    });
+
+    anime({
+      targets: innerCircle,
+      width: size + '%',
+      height: size + '%',
+      left: (posx-window.innerWidth*size/200) + 'px',
+      top: (posy-window.innerHeight*size/200) + 'px',
+      duration: 40000
+    });
+
+    anime({
+      targets: innerCircle,
+      opacity: 0,
+      delay: 6000,
+      duration: 20000
+    });
+
+    anime({
+      targets: circle,
+      opacity: 0,
+      delay: 6000,
+      duration: 14000
     })
 
     setTimeout(function() {
-      document.body.removeChild(circle)
-    }, 10000)
+      document.body.removeChild(circle);
+      document.body.removeChild(innerCircle);
+    }, 20000)
   }, 5000)
 })();
 
