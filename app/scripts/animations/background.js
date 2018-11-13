01ccaa2a326b1036fb8f645f7e6a70b0fdd655b0
Arman Yerkesh <a.yerkesh@gmail.com>
2018-11-13 12:52:18 +0300

    Add coments to background.js file

--- app/scripts/animations/background.js
+++ app/scripts/animations/background.js
@@ -1,23 +1,29 @@
 (function animateBackground() {
   setInterval(function () {
+    // Initialize circles
     var circle = document.createElement('div');
     var innerCircle = document.createElement('div');
 
+    // Add CSS class to circles
     circle.classList.add('circle');
     innerCircle.classList.add('inner-circle');
 
+    // Initialize random size and position
     var size = Math.random()*30+20;
     var posx = Math.random() * window.innerWidth;
     var posy = Math.random() * window.innerHeight;
 
+    // Append positions as CSS property
     circle.style.left = posx + 'px';
     circle.style.top = posy + 'px';
     innerCircle.style.left = posx + 'px';
     innerCircle.style.top = posy + 'px';
 
+    // Add circles to the DOM
     document.body.appendChild(circle);
     document.body.appendChild(innerCircle);
 
+    // Animate appearance
     anime({
       targets: circle,
       width: size + '%',
@@ -36,6 +42,7 @@
       duration: 40000
     });
 
+    // Animate disappearance
     anime({
       targets: innerCircle,
       opacity: 0,
@@ -50,6 +57,7 @@
       duration: 14000
     })
 
+    // Initialize new circle each 5 seconds. Remove after 20 seconds
     setTimeout(function() {
       document.body.removeChild(circle);
       document.body.removeChild(innerCircle);
