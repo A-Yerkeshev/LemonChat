64f6d8c9ae51838814b048db50b8abdd89ecb98b
Arman Yerkesh <a.yerkesh@gmail.com>
2018-11-15 12:18:04 +0300

    Attach opacity listener to submit button

--- app/scripts/animations/chatField.js
+++ app/scripts/animations/chatField.js
@@ -1,13 +1,21 @@
+// Declare event opacity listener
+function addOpacityListeners(elem, chatField) {
+  elem.addEventListener('focus', function() {
+    chatField.style.opacity = 0.9;
+  });
+
+  elem.addEventListener('blur', function() {
+    chatField.style.opacity = 0.5;
+  })
+}
+
 // Increase opacity of chat field on focus and decrease on blur
 (function opacityOnFocus() {
   var chatField = document.getElementsByClassName('chat-field')[0];
   var textArea = document.getElementsByClassName('text-area')[0];
+  var subButton = document.getElementsByClassName('sub-btn')[0];
 
-  textArea.addEventListener('focus', function() {
-    chatField.style.opacity = 0.9;
-  });
-
-  textArea.addEventListener('blur', function() {
-    chatField.style.opacity = 0.5;
-  })
+  // Bind listeners to chat field elements
+  addOpacityListeners(textArea, chatField);
+  addOpacityListeners(subButton, chatField)
 })();
