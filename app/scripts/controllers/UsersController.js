956d7f866d0b02a14c9ffc001994638ca06ad97d
Arman Yerkesh <a.yerkesh@gmail.com>
2018-11-29 13:03:32 +0300

    If username or password is incorect alert that to the user

--- app/scripts/controllers/UsersController.js
+++ app/scripts/controllers/UsersController.js
@@ -5,20 +5,22 @@ angular.module('LemonChat')
 
     // Login user on button click
     $scope.login = function(username, password) {
+      var alert = document.getElementsByClassName('log-text')[0];
       UsersService.users.forEach(function(user) {
         if (user.name == username) {
           if (user.password == password) {
             $scope.currentUser = user;
           };
         } else {
-          $window.location.href = '#!register';
+          alert.innerText = `Username or password is incorrect.
+            Maybe you forgot to register?`
         };
       })
     };
 
     // Register new user
     $scope.register = function(username, password, repPassword) {
-      var alert = document.getElementsByClassName('log-text')[0]
+      var alert = document.getElementsByClassName('log-text')[0];
       // Do not perform any action if passwords do not match
       if (password !== repPassword) {
         alert.innerText = 'Passwords do not match!'
