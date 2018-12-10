867db346399712c738ed832e5b4492ed072b0781
Arman Yerkesh <a.yerkesh@gmail.com>
2018-12-10 10:35:23 +0300

    Animate view buttons if current view is login or register

--- app/scripts/controllers/AnimationsController.js
+++ app/scripts/controllers/AnimationsController.js
@@ -1,5 +1,5 @@
 angular.module('LemonChat')
-  .controller('AnimationsController', function($scope, $window) {
+  .controller('AnimationsController', function($scope, $window, $location) {
 
     // Background animation
     $scope.animateBackground = function () {
@@ -172,10 +172,14 @@ angular.module('LemonChat')
     };
 
     $scope.$on('$viewContentLoaded', function() {
-      setTimeout(function() {
-        var rightBtn = document.getElementsByClassName('log-right-btn')[0];
-        var leftBtn = document.getElementsByClassName('log-left-btn')[0];
-        $scope.animateLogButtons(rightBtn, leftBtn);
-      }, 1000)
+      // Animate view buttons if it is login or register page
+      var path = $location.path();
+      if (path == '/login' || path == '/register') {
+        setTimeout(function() {
+          var rightBtn = document.getElementsByClassName('log-right-btn')[0];
+          var leftBtn = document.getElementsByClassName('log-left-btn')[0];
+          $scope.animateLogButtons(rightBtn, leftBtn);
+        }, 1000)
+      };
     })
   })
