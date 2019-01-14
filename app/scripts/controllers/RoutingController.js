fd81d84da1323736df78f33c266c563356bef3af
Arman Yerkesh <a.yerkesh@gmail.com>
2019-01-14 15:53:25 +0300

    Navigate via RoutingController function, instead of ng-href

--- app/scripts/controllers/RoutingController.js
+++ app/scripts/controllers/RoutingController.js
@@ -1,2 +1,7 @@
 angular.module('LemonChat')
-  .controller('RoutingController', function($scope, $location) {}
+  .controller('RoutingController', function($scope, $location, UsersService) {
+    $scope.redirect = function (path) {
+      var username = UsersService.getCurrentUser().name;
+      $location.path('/' + username + '/' + path);
+    }
+  })
