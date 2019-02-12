3c9c1e820d9b136b6299725376c3ea12b3678de2
Arman Yerkesh <a.yerkesh@gmail.com>
2019-02-12 09:30:51 +0300

    Display participants on conversation info page

--- app/scripts/controllers/ConversationsController.js
+++ app/scripts/controllers/ConversationsController.js
@@ -1,5 +1,17 @@
 angular.module('LemonChat')
   .controller('ConversationsController', function($scope, ConversationsService,
     UsersService) {
+    $scope.participants = [];
     $scope.currentConversation = ConversationsService.getCurrentConversation();
+
+    if ($scope.currentConversation) {
+      $scope.currentConversation.participants.forEach(function(participant) {
+        $scope.participants.push(UsersService.getUserByName(participant))
+      });
+    };
+
+    $scope.formatDate = function(conversation) {
+      return conversation.initializedAt.toDateString()
+    };
+
   });
